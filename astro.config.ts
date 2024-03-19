37b8ec3e2c33491f1727520e7eaa9364aaceb6ff
Jesse Janowiak <jesse@insidenewcity.com>
2024-03-18 20:55:26 -0400

    chore(config): vSCode config changes
    Added some astro packages and CZ tools

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,8 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
@@ -16,6 +18,7 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    robotsTxt(),
   ],
   markdown: {
     remarkPlugins: [
